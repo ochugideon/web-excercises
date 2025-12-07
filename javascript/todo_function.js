419bd1135c4df71e09424d46179b2603168854cb
Ochugideon <ochugideon2@gmail.com>
2025-12-07 04:20:17 +0100

    udated some functions

--- javascript/todo_function.js
+++ javascript/todo_function.js
@@ -6,11 +6,21 @@ const task = document.querySelector('input');
 
 const listItem = document.querySelector('js-listItem');
 
-function addTask() {
+
+let taskValue;
+
+function updateList(){ 
   todoListField.innerHTML = '';
-  todoList.push(task.value);
-  for (let taskValue = 0; taskValue <= todoList.length - 1; taskValue++ ) {
-    todoListField.innerHTML += `<p>${todoList[taskValue]} <button class="btn">Delete Task</button></p>`  
+    for (taskValue = 0; taskValue <= todoList.length - 1; taskValue++ ) {
+    todoListField.innerHTML += `<p>${todoList[taskValue]} <button onclick="todoList.splice(todoList.indexOf(todoList[taskValue - 1]), 1); updateList()"; class="btn">Delete Task</button></p>`  
   }
-  task.value = ''
+  task.value = '' 
+}
+
+function addTask() {
+  if (task.value){
+  todoList.push(task.value);
+  updateList()
+  } else{alert('No task was entered!')}
+  
 }
