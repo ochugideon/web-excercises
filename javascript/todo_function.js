a4b6289473603f9f570ec1c687a50eb4d299daf5
Ochugideon <ochugideon2@gmail.com>
2025-12-07 17:21:41 +0100

    added function to delete task from todo list

--- javascript/todo_function.js
+++ javascript/todo_function.js
@@ -2,25 +2,33 @@ const todoList = [];
 
 let todoListField = document.querySelector('.todo-list');
 
-const task = document.querySelector('input');
+let task = document.querySelector('.input-field');
 
 const listItem = document.querySelector('js-listItem');
 
 
-let taskValue;
-
-function updateList(){ 
+function addTask(){ 
+  let taskInList = '';
+  
   todoListField.innerHTML = '';
-    for (taskValue = 0; taskValue <= todoList.length - 1; taskValue++ ) {
-    todoListField.innerHTML += `<p>${todoList[taskValue]} <button onclick="todoList.splice(todoList.indexOf(todoList[taskValue - 1]), 1); updateList()"; class="btn">Delete Task</button></p>`  
+  
+  updateList();
+     for (let taskValue = 0; taskValue < todoList.length; taskValue++ ) {
+    html = `<p>
+    ${todoList[taskValue]}  <input type="date" class="date-feild"> <button onclick="todoList.splice(${taskValue},1); addTask()"   class="btn">Delete Task</button>
+    </p>`  
+
+    taskInList += html;
+    task.value = ''
+
+todoListField.innerHTML = taskInList;
+
   }
-  task.value = '' 
+   
 }
 
-function addTask() {
-  if (task.value){
+function updateList() {
+  if (task.value) {
   todoList.push(task.value);
-  updateList()
-  } else{alert('No task was entered!')}
-  
+}
 }
