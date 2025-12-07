07e3dcaf4e024df257a1cc1b93bf435b07853bdf
Ochugideon <ochugideon2@gmail.com>
2025-12-07 02:08:26 +0100

    created new files for todo app project

--- javascript/todo_function.js
+++ javascript/todo_function.js
@@ -1,14 +1,16 @@
 const todoList = [];
 
+let todoListField = document.querySelector('.todo-list');
+
 const task = document.querySelector('input');
 
 const listItem = document.querySelector('js-listItem');
 
 function addTask() {
+  todoListField.innerHTML = '';
   todoList.push(task.value);
-  for (x in todoList) {
-    console.log(x)
-    
+  for (let taskValue = 0; taskValue <= todoList.length - 1; taskValue++ ) {
+    todoListField.innerHTML += `<p>${todoList[taskValue]}</p>`  
   }
   task.value = ''
 }
