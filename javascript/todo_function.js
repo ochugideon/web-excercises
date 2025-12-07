ec9600e519489a11a42cde5e442ff5682aa0fb7e
Ochugideon <ochugideon2@gmail.com>
2025-12-07 18:17:05 +0100

    updated styles

--- javascript/todo_function.js
+++ javascript/todo_function.js
@@ -8,27 +8,33 @@ const listItem = document.querySelector('js-listItem');
 
 
 function addTask(){ 
-  let taskInList = '';
+  let taskInList = ''; 
   
-  todoListField.innerHTML = '';
-  
+
   updateList();
      for (let taskValue = 0; taskValue < todoList.length; taskValue++ ) {
     html = `<p>
-    ${todoList[taskValue]}  <input type="date" class="date-feild"> <button onclick="todoList.splice(${taskValue},1); addTask()"   class="btn">Delete Task</button>
+    ${todoList[taskValue]} 
+    <input type="date" class="date-feild"> <button onclick="todoList.splice(${taskValue},1); todoListField.innerHTML = ''; addTask(); "   class="btn">Delete Task</button>
     </p>`  
 
     taskInList += html;
     task.value = ''
 
 todoListField.innerHTML = taskInList;
-
   }
    
 }
 
 function updateList() {
   if (task.value) {
+  todoListField.innerHTML = '';
   todoList.push(task.value);
 }
+}
+
+function buttonEvent(event){
+  if(event.key === 'Enter') {
+    addTask()
+  }
 }
