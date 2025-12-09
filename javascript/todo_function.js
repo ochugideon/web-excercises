59382803c5c665a63b82435522ce8d3c17e51b79
Ochugideon <ochugideon2@gmail.com>
2025-12-09 15:09:40 +0100

    updated: todo_function.js and todo-styles.css

--- javascript/todo_function.js
+++ javascript/todo_function.js
@@ -1,37 +1,43 @@
-const todoList = [];
 
 let todoListField = document.querySelector('.todo-list');
+
+const dateField = document.querySelector('.date-field')
 
 let task = document.querySelector('.input-field');
 
 const listItem = document.querySelector('js-listItem');
 
+const todoList = [{name: 'wash', date: '12/04/2025'}];
 
-function addTask(){ 
-  let taskInList = ''; 
-  
-
-  updateList();
-     for (let taskValue = 0; taskValue < todoList.length; taskValue++ ) {
-    html = `<p>
-    ${todoList[taskValue]} 
-    <input type="date" class="date-feild"> <button onclick="todoList.splice(${taskValue},1); todoListField.innerHTML = ''; addTask(); "   class="btn">Remove</button>
-    </p>`  
-
-    taskInList += html;
-    task.value = ''
-
-todoListField.innerHTML = taskInList;
-  }
-   
-}
-
-function updateList() {
-  if (task.value) {
+updateList()
+function  updateList() {
   todoListField.innerHTML = '';
-  todoList.push(task.value);
+  let htmlList = '';
+  for (let taskValue = 0; taskValue < todoList.length; taskValue++ ) {
+    const listObject = todoList[taskValue]
+
+    const name = listObject.name;
+    const date = listObject.date;
+
+    html = `<div>${name}</div> <div>${date}</div> <button onclick="
+    todoList.splice(${taskValue},1); updateList()">Delete</button>`;
+    htmlList += html
+  }
+  todoListField.innerHTML = htmlList;
+  task.value = ''
+  dateField.value = ''
+  console.log(htmlList)
+  
+  }
+
+
+function addTask() {
+  if (task.value) {
+    todoList.push({name: task.value, date:dateField.value});
+    updateList()
+    }
 }
-}
+
 
 function buttonEvent(event){
   if(event.key === 'Enter') {
