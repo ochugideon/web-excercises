60e3dbcfefd0c55a2f2c022bc6fe27f852c8f076
Ochugideon <ochugideon2@gmail.com>
2025-12-11 16:00:06 +0100

    updated js

--- javascript/todo_function.js
+++ javascript/todo_function.js
@@ -1,3 +1,4 @@
+let addButton = document.querySelector('.add-btn');
 
 let todoListField = document.querySelector('.todo-list');
 
@@ -11,10 +12,7 @@ const todoList = [];
 
 updateList()
 
-
-
 function  updateList() {
-  todoListField.innerHTML = '';
 
   let htmlList = '';
 
@@ -23,29 +21,29 @@ function  updateList() {
     const name = listObject.name;
     const date = listObject.date;
 
-    html = `<div>${name}</div> <div>${date}</div> <button onclick="
-    todoList.splice(${index},1); updateList()">Delete</button>`;
+    html = `<div>${name}</div> <div>${date}</div> <button onclick="id='del'; deleteTask(); todoList.splice(${index},1);">Delete</button>`;
     htmlList += html
   })
+
   todoListField.innerHTML = htmlList;
 
   task.value = ''
   dateField.value = ''
 }
 
-  
-  // for (let taskValue = 0; taskValue < todoList.length; taskValue++ ) {
-    
-  // //console.log(htmlList)
-  
-  // }
-
 
 function addTask() {
   if (task.value) {
-    todoList.push({name: task.value, date:dateField.value});
-    updateList()
-    }
+    todoList.push({name: task.value, date:dateField.value}); 
+    task.value = '';
+    dateField.value = ''
+    setTimeout(
+    function()
+      {
+        addButton.innerText = 'Add'
+        updateList()},1000
+       );addButton.innerText = 'Loading...'
+    } else{alert('No task was entered!')}
 }
 
 
@@ -57,4 +55,8 @@ function buttonEvent(event){
 // this function is to save the list items in local storage
 function saveTask(){
   localStorage.setItem('listItems', JSON.stringify())
+}
+
+function deleteTask(){
+  let deleteBtn = document.querySelector('#del'); setTimeout(function(){deleteBtn.innerText = 'Delete'; updateList()}, 1000); deleteBtn.innerText = 'Loading...';
 }
