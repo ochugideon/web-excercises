b28f5daed180b9ce522560f8dc9ca3595e023d26
Ochugideon <ochugideon2@gmail.com>
2025-12-22 20:47:06 +0100

    updates

---
+++ amazon project/functionality.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    picture: 'images/products/IMG_7625.JPG',
+    name: 'Evans Drum Head',
+    ratings: {
+      star: 4.5,
+      rating: 87
+    },
+    price: 1090
+  },
+
+  {
+    picture: 'images/products/IMG_7624.JPG',
+    name: 'Promack Drumstick',
+    ratings: {
+      star: 3.5,
+      rating: 127
+    },
+    price: 2095
+  }
+]
